b3d755a286f043a34c59d92f185c99b86df316ac
Craig Bauer <craigbauer23@gmail.com>
2019-03-03 12:26:25 -0500

    Update doobieSlider.js

--- js/doobieSlider.js
+++ js/doobieSlider.js
@@ -1,55 +1,38 @@
 
 function doobieSlider(interval, speed) {
 
-		var wrap = $('#wrap');
-		var prev = $('.prev');
-		var next = $('.next');
-		var controls = $('.next, .prev');
+		var wrap = $('#wrap'),
+		prev = $('.prev'),
+		next = $('.next'),
+		controls = $('.next, .prev'),
 
-		//Slider Auto Play
-		var slider = setInterval(function() {
-
+		slider = setInterval(function() {
 			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
-			
 		},interval);
-
-		// Hide controls
 		controls.hide();
-
+	
 		wrap.on('mouseenter', function() {
-
 				clearInterval(slider);
-
 				$('.next, .prev').show();
-
 		});
-
+	
 		wrap.on('mouseleave', function() {
-
 			slider = setInterval(function() {
-
 			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
-			
 			},interval);
-
 			$('.next, .prev').hide();
-
 		});
 
-		//Next button
 		next.on('click', function(e) {
 			e.preventDefault();
 			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
 
 		});
 
-		//Previous button
 		prev.on('click', function(e) {
 			e.preventDefault();
 			var last = $('#wrap img:last');
-
 			last.fadeOut(speed);
-
 			setTimeout(function(){
 				$('#wrap img:last').prependTo('#wrap').fadeIn();
 			}, speed);
