6a886dd06c5cf457181c2c2b209c7c86bcf1cb7c
Craig Bauer <craigbauer23@gmail.com>
2019-03-03 12:27:15 -0500

    FIle Cleanup

--- js/doobieSlider.js
+++ js/doobieSlider.js
@@ -1,42 +1,38 @@
-
 function doobieSlider(interval, speed) {
+	var wrap = $('#wrap'),
+	prev = $('.prev'),
+	next = $('.next'),
+	controls = $('.next, .prev'),
+	slider = setInterval(function() {
+		$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
+	},interval);
+	controls.hide();
 
-		var wrap = $('#wrap'),
-		prev = $('.prev'),
-		next = $('.next'),
-		controls = $('.next, .prev'),
+	wrap.on('mouseenter', function() {
+			clearInterval(slider);
+			$('.next, .prev').show();
+	});
 
+	wrap.on('mouseleave', function() {
 		slider = setInterval(function() {
-			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
+		$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
 		},interval);
-		controls.hide();
-	
-		wrap.on('mouseenter', function() {
-				clearInterval(slider);
-				$('.next, .prev').show();
-		});
-	
-		wrap.on('mouseleave', function() {
-			slider = setInterval(function() {
-			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
-			},interval);
-			$('.next, .prev').hide();
-		});
+		$('.next, .prev').hide();
+	});
 
-		next.on('click', function(e) {
-			e.preventDefault();
-			$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
+	next.on('click', function(e) {
+		e.preventDefault();
+		$('#wrap img:first').appendTo('#wrap').hide().fadeIn(speed);
 
-		});
+	});
 
-		prev.on('click', function(e) {
-			e.preventDefault();
-			var last = $('#wrap img:last');
-			last.fadeOut(speed);
-			setTimeout(function(){
-				$('#wrap img:last').prependTo('#wrap').fadeIn();
-			}, speed);
+	prev.on('click', function(e) {
+		e.preventDefault();
+		var last = $('#wrap img:last');
+		last.fadeOut(speed);
+		setTimeout(function(){
+			$('#wrap img:last').prependTo('#wrap').fadeIn();
+		}, speed);
 
-		});
-
-}// close doobie slider
+	});
+}
